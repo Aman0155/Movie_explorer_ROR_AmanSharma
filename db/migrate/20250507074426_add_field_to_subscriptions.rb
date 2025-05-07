9f147968af8475ffba8ad69ff9d42de15fc46508
Aman Sharma <aman.sharma@magicsw.com>
2025-05-07 14:43:45 +0530

    [Aman] ADD: Completed the Stripe integration

---
+++ db/migrate/20250507074426_add_field_to_subscriptions.rb
@@ -0,0 +1,6 @@
+class AddFieldToSubscriptions < ActiveRecord::Migration[7.1]
+  def change
+    add_column :subscriptions, :expires_at, :datetime
+    add_index :subscriptions, :expires_at
+  end
+end
